a293c2b93c11cfc28840cd1f4fffa4b2b8262599
nickglu86 <enayfls@gmail.com>
2023-01-27 17:01:22 +0200

    bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 
 import { Switch, Route } from "react-router-dom";
-import { Container, Col, Row } from "react-bootstrap";
-import Account from "./Account";
-import FreeComponent from "./FreeComponent";
-import AuthComponent from "./AuthComponent";
-import ProtectedRoutes from "./ProtectedRoutes";
+import { Container, Col, Row, Stack, Button } from "react-bootstrap";
+import Account from "./components/Account";
+import FreeComponent from "./components/FreeComponent";
+import AuthComponent from "./components/AuthComponent";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <Container>
+    <Container className="my-5">
       <Row>
         <Col className="text-center">
-          <h1>Authentication App FrontEnd</h1>
+          <h1>Authentication App</h1>
 
-          <section id="navigation">
-            <a href="/">Home</a>
-            <a href="/free">Free Component</a>
-            <a href="/auth">Auth Component</a>
-          </section>
+          <Stack gap={2} className="col-md-5 mx-auto mx-5 my-5">   
+            <a  href="/">
+              <Button  className="col-md-4"  variant="outline-secondary">Home</Button>
+            </a>
+            <a href="/register">
+               <Button  className="col-md-4" variant="outline-secondary">Register</Button>
+            </a>
+            <a href="/login">
+               <Button className="col-md-4" variant="outline-secondary">Login</Button>
+            </a>
+            <a href="/free">
+             <Button className="col-md-4" variant="outline-secondary">Free Component</Button>
+            </a>
+            <a href="/auth">
+              <Button className="col-md-4" variant="outline-secondary">Auth Component</Button>
+            </a>
+          </Stack>
         </Col>
       </Row>
 
       {/* create routes here */}
       <Switch>
         <Route exact path="/" component={Account} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/free" component={FreeComponent} />
         <ProtectedRoutes path="/auth" component={AuthComponent} />
       </Switch>

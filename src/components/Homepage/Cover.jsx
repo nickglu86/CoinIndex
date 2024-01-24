30d6f7b1dd591836cc369725ab589f545464735b
nickglu86 <enayfls@gmail.com>
2024-01-24 23:27:19 +0200

    update

--- src/components/Homepage/Cover.jsx
+++ src/components/Homepage/Cover.jsx
@@ -4,25 +4,39 @@ import { Table, Col } from "react-bootstrap";
 // import { coins } from "../mockdata/coins";
 import { DataContext } from "../../context/DataContext";
 import { Globe2, GraphUpArrow, BrowserSafari } from "react-bootstrap-icons";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+const cryptos = {
+  btc: '/assets/cryptos/bitcoin.png',
+  eth: '/assets/cryptos/ethereum.png',
+  usdt: '/assets/cryptos/theter.png',
+  bnb: '/assets/cryptos/bnb.png',
+  usdc: '/assets/cryptos/usdc.webp',
+  xrp: '/assets/cryptos/xrp.png',
+  ada: '/assets/cryptos/cardano.png',
+  busd: '/assets/cryptos/busd.webp',
+}
 
 const Cover = () => {
   const { apiData, isLoading } = DataContext();
   const globalData = apiData?.globalData.data;
   const increase = globalData.market_cap_change_percentage_24h_usd.toFixed(2);
-  const marketCapLeaders =  Object.fromEntries(Object.entries(globalData.market_cap_percentage).slice(0,2))
+  const marketCapLeaders = Object.fromEntries(Object.entries(globalData.market_cap_percentage).slice(0, 2))
   const bitcoinFearIndexIMG =
-  "https://alternative.me/crypto/fear-and-greed-index.png";
+    "https://alternative.me/crypto/fear-and-greed-index.png";
+
+
 
   if (!isLoading) {
     return (
       <section className="my-2 cover" style={{ display: "flex", padding: 0 }}>
-        <Col xs={6} sm={6} md={4} lg={4} className="cb p-2">
-        <div className="cover-title">
-        <img src="/assets/icons/marketcap.png" />   
+        <Col xs={6} sm={6} md={3} lg={3} className="cb p-2">
+          <div className="cover-title">
+            <img src="/assets/icons/market.png" />
             <h2>Total Market Cap</h2>
-          </div> 
-        
-          <div>
+          </div>
+
+          <div style={{ fontSize: 24 }}>
             {"$" +
               (
                 (globalData.total_market_cap.btc *
@@ -32,41 +46,61 @@ const Cover = () => {
               " Trillion"}
           </div>
           <div>
-          <img src="/assets/icons/24hours.png"  width={30}/>   
+            <img src="/assets/icons/24hours.png" width={30} />
             {/* Last 24h:{" "} */}
-            <span style={{ color: increase > 0 ? "#16c784" : "#ea3943" }}>
+            <span style={{ color: increase > 0 ? "#16c784" : "#ea3943", fontSize: 20 }}>
               {(increase > 0 ? "+" : "") + increase + "%"}{" "}
             </span>
           </div>
         </Col>
-        <Col xs={6} sm={6} md={4} lg={4}  className="cb mx-1  p-2">
-  
-        <div className="cover-title">
-        <GraphUpArrow color="#5ca5f1" />
-            <h2>Market Cap Leaders</h2>
-          </div> 
-        <div>
-          {
-              Object.keys(marketCapLeaders).map((key, index) => (
-                <div>{key} {marketCapLeaders[key].toFixed(2)}%</div>
-              ))
-          }
-        </div>
-         
-        </Col>
-        <Col xs={6} sm={6} md={4} lg={4}  className="cb  p-2">
-        <div className="cover-title">
-             <BrowserSafari color="#5ca5f1" />
-            <h2>Fear & Greed Index</h2>
-          </div> 
+        <Col xs={6} sm={6} md={3} lg={3} className="cb mx-1  p-2">
 
-          <div style={{overflow: 'hidden'}}>
-          <img
-            className="w-50"
-            src={bitcoinFearIndexIMG}
-            alt="Latest Crypto Fear & Greed Index"
-            style={{margin: '-60px 0 -25px 0'}}
-          />
+          <div className="cover-title">
+            <img src="/assets/icons/marketcap.png" />
+            <h2>Market Cap Leaders</h2>
+          </div>
+
+          <div>
+            {
+              Object.keys(marketCapLeaders).map((key, index) => (
+                //<ProgressBar   now={marketCapLeaders[key].toFixed(0)}  label={`${marketCapLeaders[key].toFixed(0)}%`}/>
+                <div style={{display : 'flex', alignItems: 'center', justifyContent: 'space-evenly', padding: '4px 0'}}>  
+                 <img  width={30}src={cryptos[key]} /> 
+                 <ProgressBar now={marketCapLeaders[key].toFixed(0)*2} 
+                 label={`${marketCapLeaders[key].toFixed(0)}%`} 
+                 variant={key}
+                 style={{width: '80%'}}/>
+                 {/* {key} {marketCapLeaders[key].toFixed(0)}% */}
+                 </div>
+
+              ))
+            }
+          </div>
+
+        </Col>
+        <Col xs={6} sm={6} md={3} lg={3} className="cb  p-2">
+          <div className="cover-title">
+            <img src="/assets/icons/greed.png" />
+            <h2>Fear & Greed Index</h2>
+          </div>
+
+          <div style={{ overflow: 'hidden' }}>
+            <img
+              className="w-50"
+              src={bitcoinFearIndexIMG}
+              alt="Latest Crypto Fear & Greed Index"
+              style={{ margin: '-60px 0 -25px 0' }}
+            />
+          </div>
+        </Col>
+        <Col xs={6} sm={6} md={3} lg={3} className="cb  p-2">
+          <div className="cover-title">
+            <img src="/assets/icons/halving.png" />
+            <h2>Bitcoin Halving</h2>
+          </div>
+
+          <div style={{ overflow: 'hidden' }}>
+
           </div>
         </Col>
       </section>
